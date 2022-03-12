7497d72283432f8923963b7269fd42584c7207ec
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2022-03-11 23:47:26 -0300

    fix potentiometer reading issue

--- lib/ChessClock/src/Potentiometer.cpp
+++ lib/ChessClock/src/Potentiometer.cpp
@@ -13,5 +13,12 @@ uint8_t Potentiometer::getPin()
 
 int Potentiometer::getReading(int max)
 {
-    return round(map(analogRead(pin), 0, 1023, 0, max));
+    // `analogRead` goes up to 1023 but potentiometers don't always
+    // maintain this value, it might go back and forth in a range
+    // of, say, 3 to 5 numbers.
+    // Also, `map` only returns `out_max` when `x == in_max`.
+    // By setting out_max to 1024 (outside the range of analogRead)
+    // we can increase out_max by one, inherently increasing the
+    // range at which `map` will return `max`.
+    return round(map(analogRead(pin), 0, 1024, 0, max + 1));
 }
